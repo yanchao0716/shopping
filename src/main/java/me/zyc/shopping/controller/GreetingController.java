2c10986dfdce98205c6ec9627a8cc62f188c7981
zhuyanchao <yanchao0716@163.com>
2019-02-17 21:50:49 +0800

    v1.0

--- src/main/java/me/zyc/shopping/controller/GreetingController.java
+++ src/main/java/me/zyc/shopping/controller/GreetingController.java
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
+    @GetMapping("/shop")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World")String name, Model model){
         model.addAttribute("name", name);
-        return "greeting";
+        return "shop";
     }
 
 }
